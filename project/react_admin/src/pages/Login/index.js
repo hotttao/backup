ed3615abf7e9a8b148a56befaabf258d5a900b91
WardSong <1556824234@qq.com>
2020-12-13 13:23:03 +0800

    登录页面初始化

--- project/react_admin/src/pages/Login/index.js
+++ project/react_admin/src/pages/Login/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import './login.less'
+import logo from '../../assets/images/logo.svg'
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                登录组件
+            <div className="login">
+                <div className="login-header">
+                    <img src={logo} alt=""/>
+                    <h1>React 后台管理</h1>
+                </div>        
+                <div className="login-content">
+                    <h1>用户登录</h1>
+                    
+                </div>
             </div>
         )
     }
