b0730f2fa0da2aa3962ecfa1890163e81b5d90db
WardSong <1556824234@qq.com>
2020-12-20 13:27:19 +0800

    用户添加逻辑

--- project/react_admin/src/pages/User/user-form.js
+++ project/react_admin/src/pages/User/user-form.js
@@ -1,11 +1,197 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+  Form,
+  Input,
+  Tooltip,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+  Progress,
+} from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+const { Option } = Select;
 
-export default class Userform extends Component {
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+
+class RegistrationForm extends Component {
+  formRef = React.createRef()
+  constructor(props) { 
+    super(props)
+  }
+  
+  onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  validator = ()=>{ 
+    return this.formRef.current.validateFields()
+  }
+
+  prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+  
   render() {
     return (
-      <div>
-        User Form 组件
-      </div>
-    )
+      <Form
+        {...formItemLayout}
+        // form={form}
+        ref={ this.formRef }
+        name="register"
+        onFinish={this.onFinish}
+        initialValues={{
+          residence: ['zhejiang', 'hangzhou', 'xihu'],
+          prefix: '86',
+        }}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject('The two passwords that you entered do not match!');
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="nickname"
+          label={
+            <span>
+              Nickname&nbsp;
+            <Tooltip title="What do you want others to call you?">
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[
+            {
+              required: true,
+              message: 'Please input your nickname!',
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number!',
+            },
+          ]}
+        >
+          <Input
+            addonBefore={this.prefixSelector}
+            style={{
+              width: '100%',
+            }}
+          />
+        </Form.Item>
+
+      
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+        </Button>
+        </Form.Item>
+      </Form>
+    );
   }
-}
+};
+
+
+export default RegistrationForm
--- project/react_admin/src/pages/User/user.js
+++ project/react_admin/src/pages/User/user.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { Card, Button } from 'antd'
+import UserManage from '../../component/UserManage'
+import UserRegisForm from './user-form'
 
-export default class user extends Component {
+export default class User extends Component {
+  constructor(props) { 
+    super(props)
+    this.modalRef = React.createRef()
+    this.formRef = React.createRef()
+  }
+  addUser = () => { 
+    console.log(this.modalRef)
+    this.modalRef.current.showModal()
+  }
+
+  commit = () => { 
+    console.log(this.formRef.current)
+    this.formRef.current.validator()
+    return false
+  }
+
   render() {
+    const userBtn = (
+      <Button onClick={ this.addUser } type='primary'>添加用户</Button>
+    )
     return (
-      <div>
-        用户组件
-      </div>
+      <Card title={userBtn} extra='' style={{ width: 300 }}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+        
+        <UserManage ref={this.modalRef} commit={this.commit} title="添加用户">
+          <UserRegisForm ref={ this.formRef }></UserRegisForm>
+        </UserManage>
+      </Card>
     )
   }
 }
