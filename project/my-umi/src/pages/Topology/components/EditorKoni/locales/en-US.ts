1803917b33b0e1bb0c6785f7f3dbfe00246ce3de
tsong <tsong@example.com>
2022-02-16 10:06:34 +0800

    删除实验性的前端脚本

--- project/my-umi/src/pages/Topology/components/EditorKoni/locales/en-US.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  'topologyandcomponentsandeditorkoni.description':
-    'The topology diagram refers to the network structure diagram composed of network node devices and communication media',
-};
